056ba5641738ce779eafd5513f171dd1ad5a72f0
Tishalovich Maxim <tishalovichm@gmail.com>
2024-03-14 00:23:56 +0300

    Add faker config

---
+++ Faker/Faker/Config/FakerConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FakerGenerator;
+
+namespace DtoGenerator.Config;
+
+public class FakerConfig
+{
+    private Dictionary<Type, Dictionary<(string, Type), IGenerator>> _configGenerators = new();
+
+    public void Add<T, V, G>(Expression<Func<T, V>> expression)
+    {
+        ExpressionType expressionType = expression.Body.NodeType;
+        if (expressionType != ExpressionType.MemberAccess)
+        {
+            throw new FakerConfigException();
+        }
+
+        MemberExpression memberExpression = (MemberExpression)expression.Body;
+        string memberName = memberExpression.Member.Name;
+        string constructorParameterName = memberName[..1].ToLower() + memberName[1..];
+        
+        if (!_configGenerators.ContainsKey(typeof(T)))
+        {
+            _configGenerators[typeof(T)] = new Dictionary<(string, Type), IGenerator>();
+        }
+
+        _configGenerators[typeof(T)][(constructorParameterName, typeof(V))] = (IGenerator)Activator.CreateInstance(typeof(G))!;
+    }
+}
---
+++ Faker/Faker/Config/FakerConfigException.cs
@@ -0,0 +1,5 @@
+﻿namespace DtoGenerator.Config;
+
+class FakerConfigException : Exception
+{
+}
