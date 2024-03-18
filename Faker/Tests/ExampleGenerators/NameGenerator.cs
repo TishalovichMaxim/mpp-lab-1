8bdb6dfcfd3492ff4c6c7811a0a1490ec6d24d28
Tishalovich Maxim <tishalovichm@gmail.com>
2024-03-18 22:17:51 +0300

    Fix: missed test generator class

---
+++ Faker/Tests/ExampleGenerators/NameGenerator.cs
@@ -0,0 +1,13 @@
+﻿using DtoGenerator.Generator;
+using DtoGenerator;
+
+namespace Tests.ExampleGenerators;
+
+[Generator(typeof(string))]
+public class NameGenerator : IGenerator
+{
+    public object Generate(Type type, Faker faker)
+    {
+        return "SomeRandomName";
+    }
+}
