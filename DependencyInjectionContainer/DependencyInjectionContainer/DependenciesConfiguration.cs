f0ad2d48595a561f97557f33fb4a3fbe53537319
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-14 20:56:25 +0300

    Feat: basic di configuration.

---
+++ DependencyInjectionContainer/DependencyInjectionContainer/DependenciesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer;
+
+public class DependenciesConfiguration
+{
+
+    internal readonly Dictionary<Type, GenerationInfo> mapper = new(); 
+
+    public void Register(Type target, Type source,
+        GenerationType generationType = GenerationType.SINGLETON)
+    {
+        mapper[target] = new GenerationInfo(source, generationType);
+    }
+
+    public void Register<T, S>(GenerationType generationType = GenerationType.SINGLETON)
+    {
+        Register(typeof(T), typeof(S));
+    }
+
+}
+
---
+++ DependencyInjectionContainer/DependencyInjectionContainer/GenerationInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer;
+
+internal struct GenerationInfo
+{
+
+    Type Source
+    { get; }
+
+    GenerationType GenerationType
+    { get; }
+
+    public GenerationInfo(Type source, GenerationType generationType)
+    {
+        Source = source;
+        GenerationType = generationType;
+    }
+}
