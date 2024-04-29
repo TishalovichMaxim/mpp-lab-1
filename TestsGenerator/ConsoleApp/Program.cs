2072783570bc8dce8d7da2026662adb8c0ef3fed
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-29 10:02:21 +0300

    Add console app.

---
+++ TestsGenerator/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using TestsGeneratorLib;
+
+namespace ConsoleApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TestsGenerator testsGenerator = new();
+
+        Task task = testsGenerator.Generate(
+            [
+                @"C:\Users\tisha\BSUIR\sem6\Mpp\Labs\TestsGenerator\Tests\Input\ClassWithNotVoidReturnTypeMethod.cs",
+                @"C:\Users\tisha\BSUIR\sem6\Mpp\Labs\TestsGenerator\Tests\Input\ClassWithVoidReturnTypeMethod.cs",
+                @"C:\Users\tisha\BSUIR\sem6\Mpp\Labs\TestsGenerator\Tests\Input\NotSmartClass.cs",
+            ],
+            @"C:\Users\tisha\BSUIR\sem6\Mpp\Labs\TestsGenerator\Tests\Output",
+            9,
+            9,
+            9
+        );
+
+        task.Wait();
+
+        Console.WriteLine("Success");
+    }
+}
