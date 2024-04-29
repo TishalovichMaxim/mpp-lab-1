c35ae7fd7d8fa22f4bcb5e17bd4b45e8262e8379
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-29 14:32:47 +0300

    Basic configuration validator.

---
+++ DependencyInjectionContainer/DependencyInjectionContainer/ConfigurationValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer;
+
+internal class ConfigurationValidator
+{
+    private void ValidatePair(Type target, Type source)
+    {
+        if (source.IsAbstract)
+        {
+            throw new DiException("Implementation type cannot be abstract!");
+        }
+
+        if (target.IsValueType)
+        {
+            throw new DiException("Target type should be reference type!");
+        }
+    }
+
+    public void Validate(DependenciesConfiguration configuration)
+    {
+        foreach (Type target in configuration.mapper.Keys)
+        {
+            foreach (GenerationInfo info in configuration.mapper[target])
+            {
+                ValidatePair(target, info.Source);
+            }
+        }
+    }
+}
