2354327378d923285b0004c8502cb6f479b53600
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-30 12:02:15 +0300

    Part of the previous commit.

---
+++ DependencyInjectionContainer/DependencyInjectionContainer/DependencyKey.cs
@@ -0,0 +1,7 @@
+﻿namespace DependencyInjectionContainer;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public class DependencyKey(string qualifier) : Attribute
+{
+    public string Qualifier = qualifier;
+}
