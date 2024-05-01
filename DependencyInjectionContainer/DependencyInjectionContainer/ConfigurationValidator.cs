193ba5b7492344811ab6120347abe78858c3be00
Tishalovich Maxim <tishalovichm@gmail.com>
2024-05-01 10:22:39 +0300

    Add compatibility check in ConfigurationValidator.

--- DependencyInjectionContainer/DependencyInjectionContainer/ConfigurationValidator.cs
+++ DependencyInjectionContainer/DependencyInjectionContainer/ConfigurationValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +9,65 @@ namespace DependencyInjectionContainer;
 
 internal class ConfigurationValidator
 {
+    private bool CheckInterface(Type interfaceType, Type openGeneric)
+    {
+        if (interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == openGeneric)
+        {
+            return true;
+        }
+
+        return interfaceType
+            .GetInterfaces()
+            .Any(i => CheckInterface(i, openGeneric));
+    }
+
     private void ValidatePair(Type target, Type source)
     {
+        if (target.IsValueType || source.IsValueType)
+        {
+            throw new DiException("Dependency and Implementation types should be reference types!");
+        }
+        
         if (source.IsAbstract)
         {
             throw new DiException("Implementation type cannot be abstract!");
         }
 
-        if (target.IsValueType)
+        if (source.IsAssignableTo(target))
         {
-            throw new DiException("Target type should be reference type!");
+            return;
         }
+
+        if (target.IsGenericTypeDefinition && source.IsGenericTypeDefinition)
+        {
+            if (source == target)
+            {
+                return;
+            }
+        
+            if (target.IsClass)
+            {
+                Type curr = source.BaseType!;
+                while (curr != typeof(object))
+                {
+                    if (curr.IsGenericType && curr.GetGenericTypeDefinition() == target)
+                    {
+                        return;
+                    }
+
+                    curr = source.BaseType!;
+                }
+            }
+            else if (source.GetInterfaces().Any(i => CheckInterface(i, target)))
+            {
+                return;
+            }
+        }
+        
+        throw new DiException("Implementation type should be compatible with dependency type!");
     }
 
-    public void Validate(DependenciesConfiguration configuration)
+    private void ValidateCompatibility(DependenciesConfiguration configuration)
     {
         foreach (Type target in configuration.mapper.Keys)
         {
@@ -30,5 +76,30 @@ internal class ConfigurationValidator
                 ValidatePair(target, info.Source);
             }
         }
+    }
+
+    private void ValidateAmbiguous(DependenciesConfiguration configuration)
+    {
+        foreach (Type target in configuration.mapper.Keys)
+        {
+            HashSet<string?> qualifiers = new();
+            
+            foreach (GenerationInfo info in configuration.mapper[target])
+            {
+                if (!qualifiers.Add(info.Qualifier))
+                {
+                    throw new DiException($"There are several implementations of {target} with equal qualifier!");
+                }
+            }
+        }
+    }
+
+    public void Validate(DependenciesConfiguration configuration)
+    {
+        ValidateCompatibility(configuration);
+        
+        ValidateAmbiguous(configuration);
+        
+        
     }
 }
